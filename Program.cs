d1a53c777ee6413323cc43d14e51fa3e2476d7be
tobeh <tobiasch@gmx.at>
2023-02-06 00:02:21 +0100

    hi debug

--- Program.cs
+++ Program.cs
@@ -18,22 +18,29 @@ namespace Palantir_Rebirth
         static async Task Main(string[] args)
         {
             var config = JSONUtils.FromFile<BotConfig>(args[0]);
-
             while (config.Nightly && !Debugger.IsAttached)
             {
                 Thread.Sleep(1000);
             }
 
+            Console.WriteLine("hi");
             PalantirDb = new PalantirDatabase(config.PalantirDatabasePath);
+            Console.WriteLine("hi");
             Palantir = new PalantirBot(config.TokenPath, config.Nightly);
 
+            Console.WriteLine("hi");
             await Palantir.Connect();
+            Console.WriteLine("hi");
             await Palantir.SendDebugMessage("Hello there!");
+            Console.WriteLine("hi");
             await Palantir.LoadGuilds();
 
+            Console.WriteLine("hi");
             var scheduler = await QuartzUtils.GetScheduler();
+            Console.WriteLine("hi");
             await QuartzUtils.ScheduleLobbyCollector(scheduler);
 
+            Console.WriteLine("hi");
             await Task.Delay(-1);
         }
     }
